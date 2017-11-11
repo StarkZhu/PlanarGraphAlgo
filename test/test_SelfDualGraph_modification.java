9774b50322ca7f3c6d94d499584248101b74361b
qzhu3 <qzhu3@illinois.edu>
2017-11-11 11:02:59 -0600

    add test for contract operation

--- test/test_SelfDualGraph_modification.java
+++ test/test_SelfDualGraph_modification.java
@@ -222,4 +222,92 @@ public class test_SelfDualGraph_modification {
         checkIncidentListOfFace(dart.getLeft(), leftIncidentVertexID);
     }
 
+    @Test
+    public void testContractFirstEdgeOfFace() {
+        Dart dart = findDartByID(g, 13);
+        g.contractEdge(dart);
+        Assert.assertEquals(null, findDartByID(g, 12));
+        Assert.assertEquals(null, findDartByID(g, 13));
+        Vertex F = dart.getRight();
+        Vertex V = dart.getTail();
+
+        // check degree
+        Assert.assertEquals(1, F.ID);
+        Assert.assertEquals(4, F.getDegree());
+        Assert.assertEquals(5, V.ID);
+        Assert.assertEquals(7, V.getDegree());
+
+        // check local pointers
+        Dart prev = dart.getPrev();
+        Dart next = dart.getNext();
+        Dart succ = dart.getSuccessor();
+        Assert.assertEquals(20, prev.ID);
+        Assert.assertEquals(10, next.ID);
+        Assert.assertEquals(14, succ.ID);
+        Assert.assertEquals(10, prev.getNext().ID);
+        Assert.assertEquals(20, next.getPrev().ID);
+        Assert.assertEquals(8, succ.getPrev().ID);
+        Assert.assertEquals(14, succ.getPrev().getNext().ID);
+
+        Assert.assertEquals(9, succ.getPredecessor().ID);
+        Assert.assertEquals(14, succ.getPredecessor().getSuccessor().ID);
+        Assert.assertEquals(21, next.getPredecessor().ID);
+        Assert.assertEquals(10, next.getPredecessor().getSuccessor().ID);
+
+        // check incident list of V
+        int[] boundaryVertexID = new int[]{0, 2, 2, 5, 5, 3, 0};
+        checkIncidentListOfVertex(V, boundaryVertexID);
+
+
+        // check incident list of F
+        int[] rightIncidentVertexID = new int[]{3, 2, 5, 5};
+        checkIncidentListOfFace(dart.getRight(), rightIncidentVertexID);
+        int[] leftIncidentVertexID = new int[]{5, 0};
+        checkIncidentListOfFace(dart.getLeft(), leftIncidentVertexID);
+    }
+
+    @Test
+    public void testContractParallelEdge() {
+        Dart dart = findDartByID(g, 17);
+        g.contractEdge(dart);
+        Assert.assertEquals(null, findDartByID(g, 16));
+        Assert.assertEquals(null, findDartByID(g, 17));
+        Vertex F = dart.getRight();
+        Vertex V = dart.getTail();
+
+        // check degree
+        Assert.assertEquals(2, F.ID);
+        Assert.assertEquals(1, F.getDegree());
+        Assert.assertEquals(5, V.ID);
+        Assert.assertEquals(8, V.getDegree());
+
+        // check local pointers
+        Dart prev = dart.getPrev();
+        Dart next = dart.getNext();
+        Dart succ = dart.getSuccessor();
+        Assert.assertEquals(18, prev.ID);
+        Assert.assertEquals(18, next.ID);
+        Assert.assertEquals(3, succ.ID);
+        Assert.assertEquals(18, prev.getNext().ID);
+        Assert.assertEquals(18, next.getPrev().ID);
+        Assert.assertEquals(15, succ.getPrev().ID);
+        Assert.assertEquals(3, succ.getPrev().getNext().ID);
+
+        Assert.assertEquals(14, succ.getPredecessor().ID);
+        Assert.assertEquals(3, succ.getPredecessor().getSuccessor().ID);
+        Assert.assertEquals(19, next.getPredecessor().ID);
+        Assert.assertEquals(18, next.getPredecessor().getSuccessor().ID);
+
+        // check incident list of V
+        int[] boundaryVertexID = new int[]{4, 0, 0, 3, 5, 5, 5, 5};
+        checkIncidentListOfVertex(V, boundaryVertexID);
+
+
+        // check incident list of F
+        int[] rightIncidentVertexID = new int[]{5};
+        checkIncidentListOfFace(dart.getRight(), rightIncidentVertexID);
+        int[] leftIncidentVertexID = new int[]{5, 0};
+        checkIncidentListOfFace(dart.getLeft(), leftIncidentVertexID);
+    }
+
 }
