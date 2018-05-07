3a8f79aa7bef7176213fdfe96ecc1aa56a24006e
qzhu3 <qzhu3@illinois.edu>
2018-05-07 11:23:07 -0500

    run comparison on lca heuristics

--- src/util/CompareLCA.java
+++ src/util/CompareLCA.java
@@ -12,9 +12,7 @@ import java.io.*;
 import java.util.*;
 
 public class CompareLCA {
-    public static void runTest(String inputFileName, int trials, boolean rndMaxDegRoot, String outputFileName) throws FileNotFoundException {
-        SelfDualGraph g = new SelfDualGraph();
-        g.buildGraph(inputFileName);
+    public static void runTest(SelfDualGraph g, int trials, boolean rndMaxDegRoot, String outputFileName) throws FileNotFoundException {
         g.flatten();
         g.triangulate();    // g is always triangulated
 
@@ -37,7 +35,7 @@ public class CompareLCA {
         List<Vertex> rootCandidates = new ArrayList<>(vertices);
 
         PrintWriter out = new PrintWriter(outputFileName);
-        out.printf("Graph Info:\t%s\tNumber of Vertices\t%d\n", inputFileName, g.getVertexNum());
+        out.printf("Graph Info:\tx\tNumber of Vertices\t%d\n", g.getVertexNum());
         out.printf("Current run parameter:\tuse_max_degree_root = %b\n", rndMaxDegRoot);
         out.printf("\t\tExact LCA\t\tDistToRoot Heuristic\t\t\tDistToLeaf Heuristic\t\t\tCombined Heuristic\n");
         out.printf("\tSeparator Size\tBalance Ratio\tRuntime (ms)\tSeparator Size\tBalance Ratio\tRuntime (ms)\tSeparator Size\tBalance Ratio\tRuntime (ms)\tSeparator Size\tBalance Ratio\tRuntime (ms)\n");
@@ -51,7 +49,7 @@ public class CompareLCA {
 
             sp = new ModifiedFCS(g, new ExactLCA());
             testSeparator(sp, root, sb);
-            System.out.println("DistToRootHeuristic done");
+            System.out.println("ExactLCA done");
 
             sp = new ModifiedFCS(g, new DistToRootHeuristic());
             testSeparator(sp, root, sb);
@@ -77,16 +75,43 @@ public class CompareLCA {
         long endTime = System.nanoTime();
         Set<Vertex>[] subgraphs = sp.findSubgraphs();
         double ratio = Math.max(subgraphs[0].size() * 1.0 / subgraphs[1].size(), subgraphs[1].size() * 1.0 / subgraphs[0].size());
-        sb.append(String.format("\t%d\t%.5f\t%.2f", separator.size(), ratio, (endTime - startTime) / 1000000.0));
+        String str = String.format("\t%d\t%.5f\t%.2f", separator.size(), ratio, (endTime - startTime) / 1000000.0);
+        System.out.println(str);
+        sb.append(str);
     }
 
-    public static void runTest() throws FileNotFoundException {
-        String[] types = new String[]{"cylinder/rnd/4", "cylinder/symm/4", "cylinder/unsymm/4", "./grids/4", "./random/4", "./sphere/c_8"};
-        for (String type : types) {
+    public static void runTest1() throws FileNotFoundException {
+        String[] types = new String[]{"cylinder/rnd/5", "cylinder/symm/5", "cylinder/unsymm/5", "./grids/5"};
+        for (int i = 0; i < types.length; i++) {
+            String type = types[i];
+            SelfDualGraph g;
             String input = String.format("./input_data/%s.txt", type);
-            String output = String.format("./output/lcaHeuristic/4/%s.txt", type.substring(type.indexOf("/"), type.lastIndexOf("/")));
-            runTest(input, 64, false, output);
+            g = new SelfDualGraph();
+            g.buildGraph(input);
+            String output = String.format("./output/lcaHeuristic/5/%s.txt", type.substring(type.indexOf("/"), type.lastIndexOf("/")));
+            runTest(g, 32, false, output);
         }
+    }
+
+    public static void runTest2() throws FileNotFoundException {
+        SelfDualGraph g;
+        g = new SelfDualGraph();
+        g.buildGraph("./input_data/random/0.txt");
+        RandomSubgraphGenerator rsg = new RandomSubgraphGenerator(g);
+        rsg.generateRandomGraph(5);
+        runTest(g, 32, false, "./output/lcaHeuristic/5/random.txt");
+
+        g = new SelfDualGraph();
+        g.buildGraph("./input_data/sphere/c_0.txt");
+        SphereGenerator sg = new SphereGenerator(g);
+        rsg.generateRandomSubgraph(5);
+        runTest(g, 32, false, "./output/lcaHeuristic/5/sphere_c.txt");
+
+        g = new SelfDualGraph();
+        g.buildGraph("./input_data/sphere/t_0.txt");
+        sg = new SphereGenerator(g);
+        rsg.generateRandomSubgraph(5);
+        runTest(g, 32, false, "./output/lcaHeuristic/5/sphere_t.txt");
     }
 
     public static void tryRootSelection() throws FileNotFoundException {
@@ -115,7 +140,8 @@ public class CompareLCA {
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        //runTest();
-        tryRootSelection();
+        //runTest1();
+        runTest2();
+        //tryRootSelection();
     }
 }
